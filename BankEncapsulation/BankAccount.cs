0cb510491894cf2a3eef8853f7e6e8c726a4abb7
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-23 19:10:03 -0500

    Completed BankEncapsulation assignment

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() 
+        { 
+        }
+        private double balance = 0;
+
+        //Define deposit method and store value in balance field
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        // define method GetBalance to return amount stored
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much do you want to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+            
+            Console.WriteLine($"Thank you! Your current account balance is now {account.GetBalance()}");
+
+
         }
     }
 }
